21f63f341726af5b9539f3e91116ff4f914e6276
cmoneylulz <element4emerica@gmail.com>
2014-05-14 00:32:17 +0400

    Added KeyMatch.o to Makefile. TODO: soon will need to go back and refactor destructors and also my makefile for redundant instructions

--- src/KeyMatch.cpp
+++ src/KeyMatch.cpp
@@ -4,9 +4,13 @@ namespace controller
 {
     KeyMatch::KeyMatch()
     {
+        this->fileController = new FileController();
+        this->keyTable = new KeyTable();
+        this->valueTable = new ValueTable();
     }
 
     KeyMatch::~KeyMatch()
     {
+        //TODO: add complete destructors
     }
 }
