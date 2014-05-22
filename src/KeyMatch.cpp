cca1921cc912488a0467ed1d86e8e4fca7b7fbae
cmoneylulz <element4emerica@gmail.com>
2014-05-22 18:59:57 +0400

    new matching code added, preparing to begin work on generating the combined csv file

--- src/KeyMatch.cpp
+++ src/KeyMatch.cpp
@@ -21,6 +21,19 @@ namespace controller
 
     void KeyMatch::matchFiles()
     {
-        //TODO: reimplement
+        for (map<string, vector<string> >::const_iterator it = this->firstTable->getValues()->begin(); it != this->firstTable->getValues()->end(); ++it)
+        {
+            string key = it->first;
+            map<string, vector<string> >::iterator it2 = this->secondTable->getValues()->find(key);
+            if (it2 != this->secondTable->getValues()->end())
+            {
+                cout << "FOUND MATCH" << endl;
+            }
+            else
+            {
+                cout << "NO MATCH" << endl;
+            }
+        }
     }
+
 }
