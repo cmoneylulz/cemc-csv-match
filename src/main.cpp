4589acc362c3f4f62789edea84febe6779355325
cmoneylulz <element4emerica@gmail.com>
2014-05-22 17:00:54 +0400

    added code in main.cpp to resolve issue #3

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -12,8 +13,11 @@ using namespace model;
 
 int main( int argc, const char* argv[])
 {
+    //TODO: Check to see if argcount == 4
+    //TODO: Check if arg 3 & 4 are integers
+    //TODO: Check for invalid input
     if (!argv[1] == 0 && !argv[2] == 0) {
-        KeyMatch keyMatch = KeyMatch(argv[1], argv[2], 0, 0); //so nothing crashes for now
+        KeyMatch keyMatch = KeyMatch(argv[1], argv[2], atoi(argv[3]), atoi(argv[4]));
     } else {
         cout << "Invalid or no file names entered as arguments." << endl;
     }
