f68c999394962c7bc41883a55fc54536876d9dbc
cmoneylulz <element4emerica@gmail.com>
2014-06-24 18:44:07 +0400

    added comments to HelperMethods.cpp

--- src/HelperMethods.cpp
+++ src/HelperMethods.cpp
@@ -2,6 +2,13 @@
 
 namespace utility
 {
+    /** 
+     * This method splits a string based on the specified separator character
+     * and returns a vector containing all the substrings that are extracted
+     * @param aString the string to split
+     * @param delimeter the character sequence to split the string by
+     * @return a vector containing the resulting substrings
+     */
     vector<string> split(string aString, string delimeter)
     {
         int start = 0;
@@ -18,6 +25,11 @@ namespace utility
     }
     
     //TODO ERROR CHECKING HERE
+    /**
+     * This method removes quotes from a provided string
+     * @param aString the string to remove quotes from
+     * @return the string without the quotes
+     */
     string stripQuotes(string aString)
     {
         vector<string> stringTokens = split(aString, "\"");
@@ -32,6 +44,11 @@ namespace utility
         }
     }
 
+    /**
+     * This method removes spaces from a provided string
+     * @param aString the string to remove spaces from
+     * @return the string without spaces
+     */
     string stripSpaces(string aString)
     {
         vector<string> stringTokens = split(aString, "  ");
@@ -46,6 +63,11 @@ namespace utility
         }
     }
 
+    /**
+     * This method removes newline characters from a string
+     * @param aString the string to remove newlines from
+     * @return the string with no newline characters
+     */
     string stripNewLines(string aString)
     {
         vector<string> stringTokens = split(aString, "\n");
@@ -60,6 +82,11 @@ namespace utility
         }
     }
 
+    /**
+     * This method removes return characters from a string
+     * @param aString the string to remove return characters from
+     * @return the string with no return characters
+     */
     string stripReturns(string aString)
     {
         vector<string> stringTokens = split(aString, "\r");
@@ -74,8 +101,13 @@ namespace utility
         }
     }
 
-
-
+    /**
+     * This method removes all special characters from a string
+     * specifically it will remove quotes, spaces, new lines, and
+     * return characters from the specified string
+     * @param aString the string to remove characters from
+     * @return the string without special characters
+     */
     string stripAll(string aString)
     {
         string returnString = stripQuotes(aString);
