c9603cd8824e8eaa3789b826d64490aed445d75c
cmoneylulz <element4emerica@gmail.com>
2014-06-24 19:24:13 +0400

    added comments to ValueTable.cpp :

--- src/ValueTable.cpp
+++ src/ValueTable.cpp
@@ -2,21 +2,35 @@
 
 namespace model
 {
+    /**
+     * Default constructor for the ValueTable class
+     */
     ValueTable::ValueTable()
     {
         this->valueMap = new map<string, vector<string> >();    
     }
 
+    /**
+     * Default destructor for the ValueTable class
+     */
     ValueTable::~ValueTable()
     {
         delete this->valueMap;
     }
 
+    /**
+     * This method adds a new entry to the value table
+     * @param key the key for the new entry
+     * @param value the value for the new entry
+     */
     void ValueTable::addValue(const string& key, vector<string> valueList)
     {
         this->valueMap->insert(pair<string, vector<string> >(key, valueList));
     }
 
+    /**
+     * This method prints all values in the value table
+     */
     void ValueTable::printValues()
     {
         for (map<string, vector<string> >::const_iterator it = this->valueMap->begin(); it != this->valueMap->end(); ++it)
@@ -28,6 +42,9 @@ namespace model
         }
     }
 
+    /**
+     * This method is a helper method for print values, it prints all entries in a vector
+     */
     void ValueTable::printVector(vector<string> aVector)
     {
         for (vector<string>::const_iterator it = aVector.begin(); it != aVector.end(); ++it)
@@ -38,6 +55,10 @@ namespace model
         cout << endl;
     }
 
+    /**
+     * Accessor for the list of values stored by this model
+     * @return the valueMap
+     */
     map<string, vector<string> >* ValueTable::getValues()
     {
         return this->valueMap;
